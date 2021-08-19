3b1030965cbcb7387dbe8eee509d728470a971bc
pingy711 <84088183+pingy711@users.noreply.github.com>
2021-08-20 00:49:58 +0530

    Update Test_case_generator.cpp

--- Test_case_generator.cpp
+++ Test_case_generator.cpp
@@ -8,7 +8,7 @@ int main(){
     int len,b;
     std::vector<int> beads,bids;
     
-    for(int i=0;i<50;++i){      //makes 50 test cases, if you want only one test case change 50 to 1
+    for(int i=0;i<50;++i){      //makes 50 test cases, reduce to ~12 and try for ease
         
         cout<<"Test case #"<<i<<"\n";
         
@@ -25,7 +25,7 @@ int main(){
         beads.resize(len);
         
         for(int j=0;j<len;++j){
-            beads[j]=rand()%100000;
+            beads[j]=rand()%10000;
         }
         
         b=rand()%len*3;
@@ -33,7 +33,24 @@ int main(){
         bids.resize(b);
         
         for(int j=0;j<b;++j){
-            bids[j]=rand()%100000;
+            
+            if(rand()%2==0){
+                int sum=0;
+                int init=rand()%len;
+                int final=init + (rand()%10) ;
+                if(init==final){
+                    if(final==len)  init--;
+                    else final++;
+                }
+                for(int k=init;k<final;++k){
+                    if(k>=len)  break;
+                    sum+=beads[k];    
+                }
+                bids[j]=sum;
+            }
+            else{
+                bids[j]=rand()%10000;
+            }
         }
         cout<<len<<"\n";
         for(int j=0;j<len;++j)  cout<<beads[j]<<" ";
